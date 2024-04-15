7d5c47bab3f98d1fb49800b7ce789d04d76e877f
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-04-15 05:28:28 -0300

    Add files via upload

---
+++ ProcessosSimSJF/src/controller/ThreadSJF.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+public class ThreadSJF extends Thread{
+	Semaphore semaforo = new Semaphore(1);
+	int processo ;
+	private int tempo;
+	public ThreadSJF(int processo, int tempo) {
+		this.processo = processo;
+		this.tempo = tempo;
+	}
+	
+	@Override
+	public void run() {
+		try {
+		semaforo.acquire();
+		processar();
+		}catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+		}finally {
+			semaforo.release();
+		}
+		System.out.println("Processo "+processo+" finalizado em "+tempo+" segundos");
+	}
+	
+	public void processar() {
+        try {
+            Thread.sleep(tempo*1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ProcessosSimSJF/src/view/Principal.java
@@ -0,0 +1,25 @@
+package view;
+
+
+
+import java.util.Arrays;
+
+import controller.ThreadSJF;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		int [] tempo = new int[20];
+		for(int i = 0; i<20;i++) {
+			tempo[i] = (int) (Math.random()*117) + 4;
+			
+		}
+		Arrays.sort(tempo);
+		for(int j = 0;j<20;j++) {
+			Thread t = new ThreadSJF(j+1,tempo[j] );
+			t.start();
+		}
+	}
+
+}
